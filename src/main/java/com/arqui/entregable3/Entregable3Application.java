c7dd5227ba7042da6a9554d70564f8c386f7cde7
Agustin Groh <agusgroh@gmail.com>
2022-11-11 22:33:19 -0300

    Agrega driver postgres

--- src/main/java/com/arqui/entregable3/Entregable3Application.java
+++ src/main/java/com/arqui/entregable3/Entregable3Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Entregable3Application {
     public static void main(String[] args) {
         SpringApplication.run(Entregable3Application.class, args);
