3188000bd665ab85861c72873cfacfa87a35c567
Agustin Groh <agusgroh@gmail.com>
2022-11-12 08:33:58 -0300

    Arregla bugs en queries

--- src/main/java/com/arqui/entregable3/repository/PersonCareerRepository.java
+++ src/main/java/com/arqui/entregable3/repository/PersonCareerRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Repository
 public interface PersonCareerRepository extends JpaRepository<PersonCareer, PersonCareerId> {
 
-    @Query(value = "SELECT a.career_id, a.name, a.year ,SUM(a.enrolled) as enrolled,SUM(a.graduated) as graduated FROM (SELECT pc.career_id,c.name, YEAR(pc.initDate) as year, COUNT(*) as enrolled, '0' as graduated FROM PersonCareer pc JOIN Career c ON c.id=pc.career_id GROUP BY pc.career_id,c.name,year UNION SELECT pc.career_id,c.name, YEAR(pc.dueDate) as year,'0' as enrolled ,COUNT(*) as graduated FROM PersonCareer pc JOIN Career c ON c.id=pc.career_id GROUP BY pc.career_id,c.name,year HAVING year IS NOT NULL) as a GROUP BY a.career_id, a.name, a.year ORDER BY a.year ASC, a.name ASC", nativeQuery = true)
+    @Query(value = "SELECT a.career_id, a.name, a.year ,SUM(a.enrolled) as enrolled,SUM(a.graduated) as graduated FROM (SELECT pc.career_id,c.name, EXTRACT(YEAR FROM pc.init_date) as year, COUNT(*) as enrolled, '0' as graduated FROM person_career pc JOIN Career c ON c.id=pc.career_id GROUP BY pc.career_id,c.name,year UNION (SELECT pc.career_id,c.name, EXTRACT(YEAR FROM pc.due_date) AS year,'0' as enrolled ,COUNT(*) as graduated FROM person_career pc JOIN Career c ON c.id=pc.career_id GROUP BY pc.career_id,c.name,pc.due_date HAVING pc.due_date IS NOT NULL )) as a GROUP BY a.career_id, a.name, a.year ORDER BY a.year ASC, a.name ASC", nativeQuery = true)
     List<Object[]> getReport();
 
     @Query("SELECT DISTINCT(p) FROM Person p JOIN p.careers pc  WHERE pc.career.id = :careerId AND p.city = :city")
