5e5f2acd41a7c3ac63f1f19f4f3456a1501d3aa6
Agustin Groh <agusgroh@gmail.com>
2022-11-11 21:41:40 -0300

    test

--- src/main/java/com/arqui/entregable3/Entregable3Application.java
+++ src/main/java/com/arqui/entregable3/Entregable3Application.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 public class Entregable3Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Entregable3Application.class, args);
     }
