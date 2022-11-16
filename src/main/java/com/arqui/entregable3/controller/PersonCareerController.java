59ec10e9f5324448138a7c9c4a04a17b35527470
Agustin Groh <agusgroh@gmail.com>
2022-11-15 21:35:34 -0300

    Agrega anotaciones swagger

--- src/main/java/com/arqui/entregable3/controller/PersonCareerController.java
+++ src/main/java/com/arqui/entregable3/controller/PersonCareerController.java
@@ -1,10 +1,15 @@
 package com.arqui.entregable3.controller;
 
+import com.arqui.entregable3.dto.CareerDTO;
 import com.arqui.entregable3.dto.CareerReportDTO;
 import com.arqui.entregable3.dto.InscriptionDTO;
 import com.arqui.entregable3.dto.PersonDTO;
 import com.arqui.entregable3.entity.Person;
 import com.arqui.entregable3.service.PersonCareerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,16 +26,58 @@ public class PersonCareerController {
         this.personCareerService = PersonCareerService;
     }
     @RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    @Operation(
+            summary = "Inscribe un estudiante a una carrera",
+            description = "Devuelve un estudiante determinado a una carrera elegida dados por un objeto IncriptionDto.",
+            tags = { "PersonCareer-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = InscriptionDTO.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public PersonDTO addNewPerson(@RequestBody InscriptionDTO inscription){
         return personCareerService.register(inscription);
     }
 
     @RequestMapping(value ="/report",method = RequestMethod.GET, produces = "application/json")
+    @Operation(
+            summary = "Devuelve un listado de carreras",
+            description = "Devuelve un listado de carreras con sus estudiantes, especificando graduados e inscriptos.",
+            tags = { "PersonCareer-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = List.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public List<CareerReportDTO> getReport(){
         return this.personCareerService.getReport();
     }
 
     @RequestMapping(value="/{career}/{city}", method = RequestMethod.GET, produces = "application/json")
+    @Operation(
+            summary = "Devuelve un listado de personas",
+            description = "Devuelve un listado de personas por carrera y ciudad dadas por parametro.",
+            tags = { "PersonCareer-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = List.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public List<PersonDTO> getStudentsByCareerAndCity(@PathVariable int career, @PathVariable String city){
         return this.personCareerService.findAllStudentsByCareerAndCity(career,city);
     }
--- src/main/java/com/arqui/entregable3/controller/PersonController.java
+++ src/main/java/com/arqui/entregable3/controller/PersonController.java
@@ -1,13 +1,19 @@
 package com.arqui.entregable3.controller;
 
+import com.arqui.entregable3.dto.InscriptionDTO;
 import com.arqui.entregable3.dto.PersonDTO;
 import com.arqui.entregable3.entity.Person;
 import com.arqui.entregable3.service.PersonService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
+
 
 @RestController
 @RequestMapping("/person")
@@ -21,27 +27,97 @@ public class PersonController {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @Operation(
+            summary = "Devuelve todos los estudiantes",
+            description = "Devuelve todos los estudiantes inscriptos en la universidad",
+            tags = { "Person-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = List.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public List<Person> findAll(){
         System.out.println(personService.findAll());
         return personService.findAll();
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    @Operation(
+            summary = "Agrega una persona a la DB.",
+            description = "Agrega una persona con los datos enviados por el body.",
+            tags = { "Person-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDTO.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public PersonDTO addNewPerson(@RequestBody PersonDTO person){
         System.out.println("POST" + person);
         return personService.addNewPerson(person);
     }
     @RequestMapping(value="/order",method = RequestMethod.GET, produces = "application/json")
+    @Operation(
+            summary = "Devuelve un estudiante filtrado",
+            description = "Devuelve un estudiante dependiendo el parametro dado.",
+            tags = { "Person-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = List.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public List<PersonDTO> getAllStudentsBy(@RequestParam("find") String params){
         return personService.getAllStudentsBy(params);
     }
 
     @RequestMapping(value = "/collegeNotebook/{LU}", method = RequestMethod.GET, produces = "application/json")
+    @Operation(
+            summary = "Devuelve un estudiante a partir de la LU",
+            description = "Devuelve un estudiante a partir de la Libreta Universitaria dada por parametro.",
+            tags = { "Person-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = PersonDTO.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public PersonDTO getStudentByLU(@PathVariable int LU){
         return personService.getStudentByLU(LU);
     }
 
     @RequestMapping(value = "/gender/{gender}", method = RequestMethod.GET, produces = "application/json")
+    @Operation(
+            summary = "Devuelve estudiantes por genero",
+            description = "Devuelve estudiantes por genero indicado por parametro.",
+            tags = { "Person-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = List.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public List<PersonDTO> getStudentsByGender(@PathVariable String gender){
         return personService.getStudentsByGender(gender);
     }
