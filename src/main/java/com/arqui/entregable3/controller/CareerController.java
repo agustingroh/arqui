be144f604e66b7ecde56fca55937322b99c76779
Agustin Groh <agusgroh@gmail.com>
2022-11-15 21:35:34 -0300

    Agrega anotaciones swagger

--- src/main/java/com/arqui/entregable3/controller/CareerController.java
+++ src/main/java/com/arqui/entregable3/controller/CareerController.java
@@ -4,6 +4,10 @@ import com.arqui.entregable3.dto.CareerDTO;
 import com.arqui.entregable3.dto.PersonDTO;
 import com.arqui.entregable3.entity.Career;
 import com.arqui.entregable3.service.CareerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +28,20 @@ public class CareerController {
     }
 
     @RequestMapping(value = "/inscriptions",method = RequestMethod.GET, produces = "application/json")
+    @Operation(
+            summary = "Devuelve carreras que tienen estudiantes",
+            description = "Devuelve carreras que tengan estudiantes, en caso deno tener las descarta.",
+            tags = { "Career-controller" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CareerDTO.class))
+                    ),
+                    @ApiResponse(description = "Not found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+            }
+    )
     public List<CareerDTO> findAlCareersWithStudents(){
       return  careerService.getAllCareersWithStudents();
     }
